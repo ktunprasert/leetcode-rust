6fc4f4e424bb6b236608e195185f0262cd4ee687
ktunprasert <ktunprasert@outlook.com>
2025-05-26 22:06:38 +0100

    Revert "2131: lmao this is terrible"
    This reverts commit 9a350fa119230452c9c79c2865ad9e212a169360.

--- src/longestpalindrome.rs
+++ src/longestpalindrome.rs
@@ -3,27 +3,52 @@ pub fn longest_palindrome(words: Vec<String>) -> i32 {
         return 0;
     }
 
-    words
-        .iter()
-        .map(|w| w.as_bytes())
-        .map(|b| (b[0] - b'a', b[1] - b'a'))
-        .map(|(a, b)| ((a, b), (b, a)))
-        .fold(Some((0i32, [[0; 26]; 26])), |opt, ((a, b), (ra, rb))| {
-            let (mut len, mut counts) = opt.unwrap();
-            if counts[ra as usize][rb as usize] > 0 {
-                counts[ra as usize][rb as usize] -= 1;
-                len += 4;
-            } else {
-                counts[a as usize][b as usize] += 1;
+    let mut counts: [i32; 26 * 26] = [0; 26 * 26];
+    let mut len = 0i32;
+
+    fn to_key(a: u8, b: u8) -> usize {
+        a as usize * 26 + b as usize
+    }
+
+    for w in words.iter() {
+        let b = w.as_bytes();
+        let (l, r) = (b[0] - b'a', b[1] - b'a');
+        counts[to_key(l, r)] += 1;
+    }
+
+    let mut has_central_double = false;
+
+    for i in 0..26 {
+        for j in i..26 {
+            let key = to_key(i, j);
+
+            if i == j {
+                let count = counts[key];
+                if count == 0 {
+                    continue;
+                }
+
+                len += (count / 2) * 4;
+
+                if count % 2 == 1 {
+                    has_central_double = true;
+                }
+
+                continue;
             }
 
-            (len, counts).into()
-        })
-        .map(|(len, counts)| {
-            len + (0..26)
-                .find(|i| counts[*i as usize][*i as usize] > 0)
-                .map(|_| 2)
-                .unwrap_or(0)
-        })
-        .unwrap()
+            let count_ab = counts[key];
+            let count_ba = counts[to_key(j, i)];
+
+            let pairs = count_ab.min(count_ba);
+
+            len += pairs * 4;
+        }
+    }
+
+    if has_central_double {
+        len += 2;
+    }
+
+    len
 }
