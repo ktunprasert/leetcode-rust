5bf2399858b457d9f69f8555be4c5e37f0773282
ktunprasert <ktunprasert@outlook.com>
2025-05-27 18:48:57 +0100

    9_palindrome: with fixed size arr

--- src/num_palindrome.rs
+++ src/num_palindrome.rs
@@ -6,54 +6,19 @@ impl Solution {
             return x >= 0;
         }
 
-        dbg!(x);
-
-        let mut cnt = [0; 10];
-        let mut found_at = [-1i32; 10];
-        // let mut nums: &[i32] = &[];
-        let mut i = 0;
+        let mut ns = [-1; 10];
+        let mut len = 0;
 
         while x > 0 {
-            let key = (x % 10) as usize;
-
-            if cnt[key] > 0 {
-                cnt[key] -= 1;
-                found_at[key] = -1;
-                // nums.as_slice
-                // nums.push(key as i32);
-                // nums.
-            } else {
-                cnt[key] += 1;
-                found_at[key] = i;
-            }
-
-            i += 1;
+            ns[len] = x % 10;
+            len += 1;
             x /= 10;
         }
 
-        let counted: i32 = cnt.iter().sum();
-
-        // if counted == 1 {
-        //     // check middle
-        //     true
-        // } else {
-        //     counted <= 1
-        // }
-
-        // dbg!(&cnt);
-        // dbg!(&found_at);
-        for z in 0..10 {
-            if found_at[z] < 0 || cnt[z] == 0 {
-                continue;
+        for i in 0..len / 2 {
+            if ns[i] != ns[len - i - 1] {
+                return false;
             }
-
-            if i % 2 == 0 {
-                return counted <= 1;
-            }
-
-            dbg!(cnt[z]);
-            dbg!(found_at[z]);
-            return counted <= 1 && found_at[z] == ((i - 1) / 2);
         }
 
         true
